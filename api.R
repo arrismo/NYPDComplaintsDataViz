6dece499caae8c9da03a0504d54bdda381eb141f
Arris Moise <mikaelmoise00@gmail.com>
2021-08-25 14:30:11 -0400

    renamed files and cleaning

--- api.R
+++ api.R
@@ -2,7 +2,8 @@ library(curl)
 library(jsonlite)
 library(dplyr)
 res <- curl_fetch_memory("https://data.cityofnewyork.us/resource/qgea-i56i.json")
-(myjsondata <- jsonlite::prettify(rawToChar(res$content)))
-(myjsondata_df <- jsonlite::fromJSON(myjsondata) %>% 
+(nypd_data <- jsonlite::prettify(rawToChar(res$content)))
+(nypd_data <- jsonlite::fromJSON(nypd_data) %>% 
     as.data.frame())
+
 
